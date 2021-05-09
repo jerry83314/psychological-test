c1c31a72534512a7f8520ad6f4e7b58e0ce566ed
Jerry Huang <jerry83314@gmail.com>
2021-05-09 21:49:56 +0800

    安裝 axios 成功取得資料

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const url = 'https://raw.githubusercontent.com/hexschool/js-training-task/master/api/BigFive.json';
+  axios.get(url)
+    .then((res) => {
+      console.log(res.data)
+    })
   res.render('index', { title: 'Express' });
 });
 
